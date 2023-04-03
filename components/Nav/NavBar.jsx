d06abe63b13fdf5857f2312f85bfa5c0fd772fb5
rohzxz <rohzxz@gmail.com>
2023-04-03 17:50:09 +0200

    new one

--- components/Nav/NavBar.jsx
+++ components/Nav/NavBar.jsx
@@ -1,39 +1,39 @@
 import Image from "next/image";
-import React from "react";
+import React, { useRef, useState } from "react";
 import logo from "../../public/assets/logo.png";
 import Link from "next/link";
 import styles from "./Nav.module.css";
+import { AiOutlineMenu } from "react-icons/ai";
 import { socialLinks, links } from "../Data";
 
 const NavBar = () => {
   return (
-    <div>
-      <nav className="container">
-        <section className={styles.nav_bar}>
-          <div>
-            <Link href="/">
-              <Image
-                className={styles.nav_img}
-                src={logo}
-                alt="/"
-                width="60"
-                height="40"
-              />
-            </Link>
-          </div>
-          <div>
-            <ul className={styles.links}>
-              {links.map(({ id, url, text }) => {
-                return (
-                  <Link key={id} className={styles.link} href={url}>
-                    <li>{text}</li>
-                  </Link>
-                );
-              })}
-            </ul>
-          </div>
-        </section>
-      </nav>
+    <div className="container">
+      <section className={styles.nav_bar}>
+        <div>
+          <Link href="/">
+            <Image
+              className={styles.nav_img}
+              src={logo}
+              alt="/"
+              width="60"
+              height="40"
+            />
+          </Link>
+        </div>
+        <nav>
+          <ul className={styles.links}>
+            {links.map(({ id, url, text }) => {
+              return (
+                <Link key={id} className={styles.link} href={url}>
+                  <li>{text}</li>
+                </Link>
+              );
+            })}
+          </ul>
+          <AiOutlineMenu className={styles.menu_icons} />
+        </nav>
+      </section>
     </div>
   );
 };

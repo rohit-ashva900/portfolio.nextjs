8d2d07c202cebe4a6f06739ccf5d2c7c481329ae
Rohit ashva <rohzxz@gmail.com>
2024-05-19 16:38:33 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,7 @@ export default function App({ Component, pageProps }) {
     // Simulate loading delay with setTimeout
     const timeout = setTimeout(() => {
       setLoading(false);
-    }, 4000); 
+    }, 3000); 
 
     // Cleanup function
     return () => clearTimeout(timeout);
